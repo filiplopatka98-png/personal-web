b295b4cb8fbb10d9675e43ac1a65ea9bb0fa3176
Filip Lopatka <filip.lopatka@amcef.com>
2026-05-06 15:26:14 +0200

    feat: add bilingual i18n routing (SK default, EN /en/)

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,19 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: 'https://filiplopatka98-png.github.io',
+  base: '/personal-web/',
+  trailingSlash: 'always',
+  build: {
+    format: 'directory',
+  },
+  i18n: {
+    defaultLocale: 'sk',
+    locales: ['sk', 'en'],
+    routing: {
+      prefixDefaultLocale: false,
+    },
+  },
+  experimental: {},
+});
